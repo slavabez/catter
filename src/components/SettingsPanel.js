00083da7c76d353906c545708de99c0be08f5902
Slava Bezgachev <bezgachev@gmail.com>
2018-02-13 18:39:13 +0600

    Adding settings

--- src/components/SettingsPanel.js
+++ src/components/SettingsPanel.js
@@ -7,14 +7,37 @@ import {store, view} from 'react-easy-state';
 const Container = styled.div`
   display: flex;
   justify-content: center;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
 `;
 
 class SettingsPanel extends React.Component {
 
-    render(){
+    handleChangeTargetCalories(amount){
+        globalState.calorieTarget+=amount;
+    }
+
+    render() {
         return (
             <Container>
-                <h2>{dictionary[globalState.currentLanguage].changeSettings}</h2>
+                <h2>{dictionary.getWord('changeSettings')}</h2>
+
+                <div>
+                    <span>{dictionary.getWord('caloriesPerDayTarget')}</span>
+                    <span>{globalState.calorieTarget}</span>
+                    <div>
+                        <button onClick={() => this.handleChangeTargetCalories(25)}>+25</button>
+                        <button onClick={() => this.handleChangeTargetCalories(50)}>+50</button>
+                        <button onClick={() => this.handleChangeTargetCalories(-25)}>-25</button>
+                        <button onClick={() => this.handleChangeTargetCalories(-50)}>-50</button>
+                    </div>
+                </div>
+
+                <div>
+                    <span>{dictionary}</span>
+                </div>
+
             </Container>
         );
     }
--- src/dictionary.js
+++ src/dictionary.js
@@ -1,10 +1,17 @@
+import globalState from './globalState';
+
 export default {
     en: {
         clickToExpand: 'Click to expand',
-        changeSettings: 'Adjust values'
+        changeSettings: 'Adjust values',
+        caloriesPerDayTarget: 'Daily kCal target'
     },
     ru: {
         clickToExpand: 'Otkryt menyu',
-        changeSettings: 'Pomenyat dannye'
+        changeSettings: 'Pomenyat dannye',
+        caloriesPerDayTarget: 'Cel kalorii v den'
+    },
+    getWord(word){
+        return this[globalState.currentLanguage][word];
     }
 };
