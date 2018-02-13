8d08d4e3c2349dba2fb7262e6d083416f98a91db
Slava Bezgachev <bezgachev@gmail.com>
2018-02-13 16:24:52 +0600

    Initial add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AppWrapper from './styled/AppWrapper';
+
+export default class App extends React.Component {
+    render(){
+        return (
+            <AppWrapper>
+                App
+            </AppWrapper>
+        );
+    }
+}
